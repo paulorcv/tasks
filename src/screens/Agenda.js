b23b71295e11ac6fe65480e0686097276f8c54fe
Paulo Campos <paulorcv@gmail.com>
2019-08-18 14:24:41 -0300

    Toggle tasks

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -77,6 +77,18 @@ export class Agenda extends Component {
       },
     ],
   };
+
+  toggleTask = id => {
+    const tasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task = { ...task };
+        task.doneAt = task.doneAt ? null : new Date();
+      }
+      return task;
+    });
+    this.setState({ tasks });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -94,7 +106,9 @@ export class Agenda extends Component {
           <FlatList
             data={this.state.tasks}
             keyExtractor={item => `${item.id}`}
-            renderItem={({ item }) => <Task {...item} />}
+            renderItem={({ item }) => (
+              <Task {...item} toggleTask={this.toggleTask} />
+            )}
           />
         </View>
       </View>
