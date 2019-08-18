722032035ae5eeb65f14d137f394da6d14c52aa0
Paulo Campos <paulorcv@gmail.com>
2019-08-18 14:09:55 -0300

    Flatlist component to show tasks at Agenda

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  FlatList,
+} from 'react-native';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import todayImage from '../../assets/imgs/today.jpg';
@@ -7,6 +13,70 @@ import commonStyles from '../commonStyles';
 import Task from '../components/Task';
 
 export class Agenda extends Component {
+  state = {
+    tasks: [
+      {
+        id: Math.random(),
+        desc: 'Ler códigos Github',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Concluir o curso',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Ler códigos Github',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Concluir o curso',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Ler códigos Github',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Concluir o curso',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Ler códigos Github',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Concluir o curso',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Ler códigos Github',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Concluir o curso',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+    ],
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -21,41 +91,10 @@ export class Agenda extends Component {
           </View>
         </ImageBackground>
         <View style={styles.tasksContainer}>
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
-          />
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
-          />
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
-          />
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
-          />
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
-          />
-          <Task desc='Tarefa pendente' estimateAt={new Date()} doneAt={null} />
-          <Task
-            desc='Tarefa concluída'
-            estimateAt={new Date()}
-            doneAt={new Date()}
+          <FlatList
+            data={this.state.tasks}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({ item }) => <Task {...item} />}
           />
         </View>
       </View>
